8a9542864eb4b27d976bdf8d8d423fbd25fa5b8d
dad2jrn <dad2jrn@gmail.com>
2023-04-02 17:28:17 -0400

    refactor for simplicity

---
+++ getlength.py
@@ -0,0 +1,4 @@
+
+def pwd_length():
+    length = input("Please chose the lenght of your password (recommended lenght is 12 or more).  ")
+    return length
--- main.py
+++ main.py
@@ -1,13 +1,18 @@
 import secrets, string
+from getlength import pwd_length
+
 
 def passgen(length):
     letters = string.ascii_letters
     digits = string.digits
     special_chars = string.punctuation
     alphabet = letters + digits + special_chars
+    pwd = ""
+    for i in range(int(length)):
+        pwd += "".join(secrets.choice(alphabet))
+    return pwd
 
-    pwd = ''
-    for i in range(int(pwd_length)):
-    pwd += ''.join(secrets.choice(alphabet))
 
-    return pwd)
+if __name__ == "__main__":
+    length = pwd_length()
+    print(passgen(length))
