b0e45006cec79e5ab480240e2bb4a3a49ceea59f
dad2jrn <dad2jrn@gmail.com>
2023-04-02 17:16:25 -0400

    convert to method

--- main.py
+++ main.py
@@ -1,15 +1,13 @@
 import secrets, string
 
-# store string module constants in vars
-letters = string.ascii_letters
-digits = string.digits
-special_chars = string.punctuation
-alphabet = letters + digits + special_chars
+def passgen(length):
+    letters = string.ascii_letters
+    digits = string.digits
+    special_chars = string.punctuation
+    alphabet = letters + digits + special_chars
 
-pwd_length = input("Please chose the lenght of your password (recommended lenght is 12 or more).  ")
+    pwd = ''
+    for i in range(int(pwd_length)):
+    pwd += ''.join(secrets.choice(alphabet))
 
-pwd = ''
-for i in range(int(pwd_length)):
-  pwd += ''.join(secrets.choice(alphabet))
-
-print(pwd)
+    return pwd)
