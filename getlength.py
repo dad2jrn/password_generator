421596079d652520ede16d3dd114bc29a0cd61bc
dad2jrn <dad2jrn@gmail.com>
2023-04-02 17:29:06 -0400

    spelling

--- getlength.py
+++ getlength.py
@@ -1,4 +1,4 @@
 
 def pwd_length():
-    length = input("Please chose the lenght of your password (recommended lenght is 12 or more).  ")
+    length = input("Please chose the lenght of your password (recommended minimum length is 12 or more).  ")
     return length
