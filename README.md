f3d2bd5d30fdbbec2144ed94858a59968758a61d
dad2jrn <dad2jrn@gmail.com>
2023-04-02 15:09:57 -0400

    password genereator

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
+[![Code style: black](https://img.shields.io/badge/code%20style-black-000000.svg)](https://github.com/psf/black) [![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0) 
+
+
 # password_generator
 Just a simple app that will generate a strong password for the user based on some user input
+
--- app.py
+++ app.py
@@ -1,2 +1,17 @@
-import random
+import random, 
 
+print("Password Generator")
+
+chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890~`!@#$%^&*()_-+={[}]|:;'<,>.?/"
+
+def passgen():
+    password = ''
+    length = input("How many characters for your password? ")
+    plength = int(length)
+    print(f"Here is your new {plength} character password")
+    for c in range(plength):
+        password += random.choice(chars)
+    return password
+
+
+print(passgen())
