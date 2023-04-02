2492f57c0363ba4d89d0d68d053374ccdaca224e
dad2jrn <dad2jrn@gmail.com>
2023-04-02 17:14:58 -0400

    output password

--- main.py
+++ main.py
@@ -4,9 +4,12 @@ import secrets, string
 letters = string.ascii_letters
 digits = string.digits
 special_chars = string.punctuation
-
-# wrap string constants into a single var
 alphabet = letters + digits + special_chars
 
 pwd_length = input("Please chose the lenght of your password (recommended lenght is 12 or more).  ")
 
+pwd = ''
+for i in range(int(pwd_length)):
+  pwd += ''.join(secrets.choice(alphabet))
+
+print(pwd)
